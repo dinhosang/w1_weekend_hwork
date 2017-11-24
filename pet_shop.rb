17333c304e7487cafe598765d27a1e63b0247617
Douglas Sangster <d.sangster@hotmail.co.uk>
2017-11-24 14:17:15 +0000

    add_or_remove_cash function completed

--- pet_shop.rb
+++ pet_shop.rb
@@ -3,7 +3,12 @@ def pet_shop_name(shop_details)
   return shop_name
 end
 
+
 def total_cash(shop_details)
   sum_cash = shop_details[:admin][:total_cash]
   return sum_cash
+end
+
+def add_or_remove_cash(shop_details, amount)
+  shop_details[:admin][:total_cash] += amount
 end
